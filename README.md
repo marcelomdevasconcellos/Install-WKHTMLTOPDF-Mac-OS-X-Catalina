be5789db87dc3e90c91c71a3544919d529f82789
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-07-10 19:15:31 -0300

    shell script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Install-WKHTMLTOPDF-Mac-OS-X-Catalina
+
 Install-WKHTMLTOPDF-Mac-OS-X-Catalina
+
+# Credits #
+
+* http://macappstore.org/wkhtmltopdf/
---
+++ install-wkhtmltopdf-mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+brew cask install wkhtmltopdf
